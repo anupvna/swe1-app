30519d34bb536b331e89c090ff530950be40c3db
Anoop <avn2049@nyu.edu>
2026-03-06 15:44:18 -0500

    Added test to fix coverage badge

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,6 @@
-# from django.test import TestCase
+from django.test import TestCase
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        """A simple test to ensure the CI pipeline tracks coverage."""
+        self.assertEqual(1 + 1, 2)
